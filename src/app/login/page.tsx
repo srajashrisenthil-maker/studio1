4e43cfe2ac97ee302368c4f32a49e777abec10c7
Rajashri S <srajashrisenthil@gmail.com>
2025-09-13 06:06:16 +0000

    ⨯ useSearchParams() should be wrapped in a suspense boundary at page "/l

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 
+import { Suspense } from 'react';
 import { UserAuthForm } from "@/components/auth/user-auth-form";
 import { useSearchParams } from "next/navigation";
 import { Leaf } from "lucide-react";
 
-export default function LoginPage() {
+function Login() {
   const searchParams = useSearchParams();
   const role = searchParams.get('role') === 'marketman' ? 'marketman' : 'farmer';
 
+  return (
+    <div className="lg:p-8">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight font-headline">
+            {role === 'farmer' ? "Farmer" : "Marketman"} Portal
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your details to access your dashboard
+          </p>
+        </div>
+        <UserAuthForm role={role} />
+      </div>
+    </div>
+  );
+}
+
+
+export default function LoginPage() {
   return (
     <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
@@ -30,19 +50,9 @@ export default function LoginPage() {
             </blockquote>
           </div>
        </div>
-       <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight font-headline">
-              {role === 'farmer' ? "Farmer" : "Marketman"} Portal
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your details to access your dashboard
-            </p>
-          </div>
-          <UserAuthForm role={role} />
-        </div>
-      </div>
+       <Suspense fallback={<div>Loading...</div>}>
+        <Login />
+      </Suspense>
     </div>
   );
 }
